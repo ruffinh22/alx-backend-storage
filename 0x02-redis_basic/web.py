13030a920de5109a124160bef20523167daf553f
ruffin hounsounnon <ruffinhounsounnon@gmail.com>
2023-07-21 08:10:03 +0100

    update web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-create a web cach
+create a web cache
 """
 import redis
 import requests
@@ -9,32 +9,30 @@ from functools import wraps
 rc = redis.Redis()
 
 def cache_page(func):
-    """ get a page and cach value"""
     @wraps(func)
     def wrapper(url):
+        # Check if the page is already cached
         cached_value = rc.get(f"cached:{url}")
         if cached_value is not None:
+            print("Using cached content...")
             return cached_value.decode('utf-8')
 
-        page_content = func(url)
-
-        # Cache the page content with a 10-second expiration
-        rc.setex(f"cached:{url}", 10, page_content)
+        # Fetch the page content and cache it
+        resp = requests.get(url)
+        rc.setex(f"cached:{url}", 10, resp.text)
 
         # Increment the access count for the URL
         rc.incr(f"count:{url}")
 
-        return page_content
+        return resp.text
 
     return wrapper
 
 @cache_page
 def get_page(url: str) -> str:
     """ Get a page and cache the value"""
-    resp = requests.get(url)
-    return resp.text
+    return requests.get(url).text
 
 if __name__ == "__main__":
-    # Test the get_page function with caching
-    page_content = get_page('http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com')
+    page_content = get_page('http://slowwly.robertomurray.co.uk')
     print(page_content)
