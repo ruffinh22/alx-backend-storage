8767f6d465626d036715f068980dd18719d28dab
ruffin hounsounnon <ruffinhounsounnon@gmail.com>
2023-07-21 07:59:31 +0100

    update web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 """
-create a web cach
+create a web cache
 """
 import redis
 import requests
 rc = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
+    """ get a page and cache value"""
+    cached_value = rc.get(f"cached:{url}")
+    if cached_value is not None:
+        return cached_value.decode('utf-8')
+
     resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    rc.setex(f"cached:{url}", 10, resp.text)  # Cache the page content with a 10-second expiration
+    rc.incr(f"count:{url}")  # Increment the access count for the URL
     return resp.text
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    page_content = get_page('http://slowwly.robertomurray.co.uk')
+    print(page_content)
